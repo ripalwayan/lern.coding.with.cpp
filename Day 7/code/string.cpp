66071fd2ed1e9a0cb4e6c363debaf526ace58cf9
ripalwayan <ripalrusli09@gmail.com>
2025-06-14 22:06:37 +0800

    Day 7 update

---
+++ Day 7/code/string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string nameUser;
+  cout << "[+] Masukkan nama anda : "; getline(cin, nameUser);
+  cout << "[=] " << nameUser << endl;
+
+
+  return 0;
+}
